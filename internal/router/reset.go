0f9cd3c3b977eeb84209a5a6e9c5dc8285f88f25
Justin Arnold <hello@justin-arnold.com>
2025-04-22 23:46:15 -0400

    refactor: Update reset route for sessions

--- internal/router/reset.go
+++ internal/router/reset.go
@@ -1,23 +1,13 @@
 package router
 
 import (
-	"encoding/json"
 	"net/http"
+
+	"github.com/Justin-Arnold/go-api-dungeon/internal/session"
 )
 
 func HandleReset(w http.ResponseWriter, r *http.Request) {
-
-	if r.Header.Get("Accept") == "application/json" {
-		w.Header().Set("Content-Type", "application/json")
-
-		data := map[string]string{}
-
-		if err := json.NewEncoder(w).Encode(data); err != nil {
-			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
-			return
-		}
-		return
-	}
+	session.SetGameState(w, r, session.GameState{})
 
 	RenderTemplate(w, "reset", nil)
 }
