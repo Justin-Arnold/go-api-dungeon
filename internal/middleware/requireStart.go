89d20359c41b0fd714169c9244658cb4ef477be1
Justin Arnold <hello@justin-arnold.com>
2025-04-22 23:35:28 -0400

    refactor: Update start middleware for sessions

--- internal/middleware/requireStart.go
+++ internal/middleware/requireStart.go
@@ -2,31 +2,27 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/Justin-Arnold/go-api-dungeon/internal/session"
 )
 
 func RequireStart(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Header.Get("Accept") == "application/json" {
-			gameState, ok := GetGameState(r.Context())
-			if !ok {
-				// Handle the case where game state is not available
-				http.Error(w, "Game state not available", http.StatusBadRequest)
-				return
-			}
-
-			// check if "start" is in the completed rooms
-			for _, room := range gameState.CompletedRooms {
-				if room == "start" {
-					// If the room is completed, allow the request to proceed
-					next.ServeHTTP(w, r)
-					return
-				}
-			}
-
-			RedirectToError(w, r, "not-started", http.StatusTemporaryRedirect)
+		state, error := session.GetGameState(r)
+		if error != nil {
+			http.Error(w, "Game state not found", http.StatusInternalServerError)
+			return
 		}
 
-		next.ServeHTTP(w, r)
+		// TODO use slices.contains instead of a loop
+		for _, room := range state.CompletedRooms {
+			if room == "start" {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		RedirectToError(w, r, "not-started", http.StatusTemporaryRedirect)
 	})
 }
