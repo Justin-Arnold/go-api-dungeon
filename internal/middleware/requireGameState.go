ac653087544a4416a6063e5ac17d658d206942cc
Justin Arnold <hello@justin-arnold.com>
2025-04-22 23:30:18 -0400

    refactor: Update the game state middleware for sessions

--- internal/middleware/requireGameState.go
+++ internal/middleware/requireGameState.go
@@ -1,12 +1,10 @@
 package middleware
 
 import (
-	"context"
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/Justin-Arnold/go-api-dungeon/internal/dungeon"
+	"github.com/Justin-Arnold/go-api-dungeon/internal/session"
 )
 
 // GameState holds the current game state data
@@ -23,48 +21,18 @@ type GameState struct {
 	CurrentEnemyHP     int
 	CurrentEnemyMaxHP  int
 	CompletedRooms     []string
+	EventDescription   string
+	EventChoices       []dungeon.Option
 }
-
-// Define a custom type for context keys
-type contextKey string
-
-// GameStateKey is the key used to store the game state in the context
-const GameStateKey contextKey = "gameState"
 
 func RequireGameState(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		convertedCharacterDamage, err := strconv.Atoi(r.Header.Get("X-Character-Damage"))
-		if err != nil {
-			convertedCharacterDamage = 0
-		}
-		convertedCurrentEnemyHP, err := strconv.Atoi(r.Header.Get("X-Current-Enemy-Hp"))
-		if err != nil {
-			convertedCurrentEnemyHP = 0
+
+		gameState, error := session.GetGameState(r)
+		if error != nil {
+			session.SetGameState(w, r, gameState)
 		}
 
-		// log.Printf("DEBUG: Request headers: %+v", r.Header)
-		// Only check game state for API requests
-		if r.Header.Get("Accept") == "application/json" {
-			gameState := GameState{
-				CharacterName:   r.Header.Get("X-Character-Name"),
-				CharacterClass:  r.Header.Get("X-Character-Class"),
-				CurrentRoom:     r.Header.Get("X-Current-Room"),
-				CharacterDamage: convertedCharacterDamage,
-				CurrentEnemyHP:  convertedCurrentEnemyHP,
-				CompletedRooms:  strings.Split(r.Header.Get("X-Completed-Rooms"), ","),
-			}
-			// Add the game state to the request context
-			ctx := context.WithValue(r.Context(), GameStateKey, gameState)
-			next.ServeHTTP(w, r.WithContext(ctx))
-			return
-		}
-
-		// For non-API requests, just pass through
 		next.ServeHTTP(w, r)
 	})
-}
-
-func GetGameState(ctx context.Context) (GameState, bool) {
-	gameState, ok := ctx.Value(GameStateKey).(GameState)
-	return gameState, ok
 }
