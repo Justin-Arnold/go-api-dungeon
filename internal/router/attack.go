4d8ba303ada1a014f2b72b3e8af9963d91e10527
Justin Arnold <hello@justin-arnold.com>
2025-04-22 23:35:48 -0400

    refactor: Update attack route for sessions

--- internal/router/attack.go
+++ internal/router/attack.go
@@ -1,39 +1,27 @@
 package router
 
 import (
-	"encoding/json"
 	"net/http"
 
-	"github.com/Justin-Arnold/go-api-dungeon/internal/middleware"
+	"github.com/Justin-Arnold/go-api-dungeon/internal/session"
 )
 
 func HandleAttack(w http.ResponseWriter, r *http.Request) {
 
-	if r.Header.Get("Accept") == "application/json" {
-		w.Header().Set("Content-Type", "application/json")
-
-		gameState, ok := middleware.GetGameState(r.Context())
-		if !ok {
-			// Handle the case where game state is not available
-			http.Error(w, "Game state not available", http.StatusBadRequest)
-			return
-		}
-
-		newEnemyHP := gameState.CurrentEnemyHP - gameState.CharacterDamage
-
-		if newEnemyHP <= 0 {
-			newEnemyHP = 0
-			gameState.CompletedRooms = append(gameState.CompletedRooms, gameState.CurrentRoom)
-		}
-
-		gameState.CurrentEnemyHP = newEnemyHP
-
-		if err := json.NewEncoder(w).Encode(gameState); err != nil {
-			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
-			return
-		}
+	state, error := session.GetGameState(r)
+	if error != nil {
+		http.Error(w, "Game state not found", http.StatusInternalServerError)
 		return
 	}
+
+	newEnemyHP := state.CurrentEnemyHP - state.CharacterDamage
+
+	if newEnemyHP <= 0 {
+		newEnemyHP = 0
+		state.CompletedRooms = append(state.CompletedRooms, state.CurrentRoom)
+	}
+
+	state.CurrentEnemyHP = newEnemyHP
 
 	RenderTemplate(w, "attack", &TemplateData{})
 }
